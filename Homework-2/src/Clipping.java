0042349db98ec3d45b6811913286e2b96bcd307b
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2018-02-11 16:51:19 -0500

    Homework done

--- Homework-2/src/Clipping.java
+++ Homework-2/src/Clipping.java
@@ -14,7 +14,7 @@
 
 /**
  *
- * @author helmuthtrefftz
+ * @author helmuthtrefftz and bodhert
  */
 
 import java.awt.Color;
@@ -29,10 +29,6 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 
 
-/**
- *
- * @author helmuthtrefftz
- */
 public class Clipping extends JPanel implements KeyListener  {
 
     int w = 0;
@@ -49,6 +45,12 @@ public class Clipping extends JPanel implements KeyListener  {
     int globalYmin ;
     int globalXmax ;
     int globalYmax ;
+    
+    static int INSIDE = 0; // 0000
+    static int LEFT = 1;   // 0001
+    static int RIGHT = 2;  // 0010
+    static int BOTTOM = 4; // 0100
+    static int TOP = 8;    // 1000
     
     double theta = 0;
     double step = 1.0 * Math.PI / 180d;
@@ -81,12 +83,20 @@ public class Clipping extends JPanel implements KeyListener  {
         w = size.width - insets.left - insets.right;
         h = size.height - insets.top - insets.bottom;
 
-
-        
-        //drawConnectionGraph();
         drawClippingRectangle(minX, minY, maxX, maxY);
-        drawTransformedLine(0, 0, (int)(radius * Math.cos(theta)), 
-                (int)(radius * Math.sin(theta)));
+//        drawTransformedLine(0, 0, (int)(radius * Math.cos(theta)), 
+//                (int)(radius * Math.sin(theta)));
+        
+        int xmin = transformX(0); int ymin = transformY(0);
+        int xmax = transformX((int)(radius * Math.cos(theta)));
+        int ymax = transformY((int)(radius * Math.sin(theta)));
+        
+        g2d.setColor(Color.green);
+        drawBresenham(g2d, xmin, ymin, xmax, ymax);
+        
+        CohenSutherlandLineClipAndDraw(g2d, xmin, ymin,xmax, ymax);
+        
+        
     }
     
     public void drawClippingRectangle(int minX, int minY, int maxX, int maxY) {
@@ -102,7 +112,89 @@ public class Clipping extends JPanel implements KeyListener  {
         globalXmax = x1 + w1;
         globalYmax = y1 + h1;
         
-        System.out.println(" " + x1 + " " + y1 + " " + w1 + " " + h1);
+//        System.out.println(" " + x1 + " " + y1 + " " + w1 + " " + h1);
+    }
+    
+    public int ComputeOutCode(double x,double y)
+    {
+        int code = INSIDE;
+        
+        if (x < globalXmin)           // to the left of clip window
+		code |= LEFT;
+	else if (x > globalXmax)      // to the right of clip window
+		code |= RIGHT;
+	if (y < globalYmin)           // below the clip window
+		code |= BOTTOM;
+	else if (y > globalYmax)      // above the clip window
+		code |= TOP;
+        
+	return code;
+                
+    }
+    
+    void CohenSutherlandLineClipAndDraw(Graphics2D g2d,int x0, int y0, int x1, int y1)
+    {
+        int outcode0 = ComputeOutCode(x0, y0);
+	int outcode1 = ComputeOutCode(x1, y1);
+	boolean accept = false;
+
+	while (true) 
+        {
+		if ((outcode0 | outcode1) == 0) 
+                {
+			// bitwise OR is 0: both points inside window; trivially accept and exit loop
+			accept = true;
+			break;
+		} 
+                else if ((outcode0 & outcode1) != 0) 
+                {
+			// bitwise AND is not 0: both points share an outside zone (LEFT, RIGHT, TOP,
+			// or BOTTOM), so both must be outside window; exit loop (accept is false)
+			break;
+		} 
+                else 
+                {
+			
+			int x = 0, y = 0;
+			int outcodeOut = outcode0 != 0 ? outcode0 : outcode1;
+
+			if ((outcodeOut & TOP) != 0) 
+                        {           // point is above the clip window
+				x = x0 + (x1 - x0) * (globalYmax - y0) / (y1 - y0);
+				y = globalYmax;
+			} 
+                        else if ((outcodeOut & BOTTOM) != 0) 
+                        { // point is below the clip window
+				x = x0 + (x1 - x0) * (globalYmin - y0) / (y1 - y0);
+				y = globalYmin;
+			} else if ((outcodeOut & RIGHT) != 0) 
+                        {  // point is to the right of clip window
+				y = y0 + (y1 - y0) * (globalXmax - x0) / (x1 - x0);
+				x = globalXmax;
+			} 
+                        else if ((outcodeOut & LEFT) != 0) 
+                        {   // point is to the left of clip window
+				y = y0 + (y1 - y0) * (globalXmin - x0) / (x1 - x0);
+				x = globalXmin;
+			}
+			// Now we move outside point to intersection point to clip
+			// and get ready for next pass.
+			if (outcodeOut == outcode0) {
+				x0 = x;
+				y0 = y;
+				outcode0 = ComputeOutCode(x0, y0);
+			} else {
+				x1 = x;
+				y1 = y;
+				outcode1 = ComputeOutCode(x1, y1);
+			}
+		}
+	}
+	if (accept) {
+//		DrawRectangle(globalXmin, globalYmin, globalXmax, globalYmax);  
+                g2d.setColor(Color.red);
+		drawBresenham(g2d,x0, y0, x1, y1);
+	}
     }
     
     public void drawTransformedLine(int x1, int y1, int x2, int y2) {
@@ -112,8 +204,9 @@ public class Clipping extends JPanel implements KeyListener  {
         int y1t = transformY(y1);
         int x2t = transformX(x2);
         int y2t = transformY(y2);
+        System.out.println(x1t + " " + y1t + " " + x2t + " " + y2t + " ");
 //        g2d.drawLine(x1t, y1t, x2t, y2t);
-        drawBresenham(g2d, x1t, y1t, x2t, y2t);
+//        drawBresenham(g2d, x1t, y1t, x2t, y2t);
     }
     
     public int transformX(int x) {
@@ -164,16 +257,7 @@ public class Clipping extends JPanel implements KeyListener  {
         if (dx >= dy) 
         {
             while (true) 
-            {
-                System.out.println("x:" + transformX(x) + " y:" + transformY(y));
-                int tempY = transformY(y);
-                System.out.println("..." + globalXmin + " " + globalXmax + " " + globalYmin + " " + globalYmax);
-                  if(x >= globalXmin && x <= globalXmax && y >= globalYmin && y <= globalYmax){ 
-                    g2d.setColor(Color.red);
-                    System.out.println("entro 1");
-                }
-                else g2d.setColor(Color.green);
-                
+            {                
                 g2d.drawOval(x, y, pixelSize,pixelSize);
                 if (x == x2)
                     break;
@@ -190,11 +274,6 @@ public class Clipping extends JPanel implements KeyListener  {
         {
             while (true) 
             {
-             
-                 if(x >= globalXmin && x <= globalXmax && y >= globalYmin && y <= globalYmax){ 
-                    g2d.setColor(Color.red);
-                    System.out.println("entro 2");
-                }
                 g2d.drawOval(x, y, pixelSize,pixelSize);
                 if (y == y2) break;
                 y += iy;
